b142f38f5bc025011ddd06913bedf15c0b5a455d
Radhika <mails2radhika.vk@gmail.com>
2024-06-09 17:21:29 +0100

    Added space

--- src/main/java/thirdparty/paymentgateway/TicketPaymentServiceImpl.java
+++ src/main/java/thirdparty/paymentgateway/TicketPaymentServiceImpl.java
@@ -5,6 +5,7 @@ public class TicketPaymentServiceImpl implements TicketPaymentService {
     @Override
     public void makePayment(long accountId, int totalAmountToPay) {
         // Real implementation omitted, assume working code will take the payment using a card pre linked to the account.
+
     }
 
 }
